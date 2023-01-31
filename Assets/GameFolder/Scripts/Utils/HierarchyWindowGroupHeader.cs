17b15924684174f418db3af84c9a49a4553d862f
Kevin Grandez <kevinlg08@gmail.com>
2023-01-31 15:04:43 -0300

    Template VR Quest 2 Game

---
+++ Assets/GameFolder/Scripts/Utils/HierarchyWindowGroupHeader.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Hierarchy Window Group Header
+/// http://diegogiacomelli.com.br/unitytips-hierarchy-window-group-header
+/// </summary>
+[InitializeOnLoad]
+public static class HierarchyWindowGroupHeader
+{
+    static HierarchyWindowGroupHeader()
+    {
+        EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;
+    }
+
+    static void HierarchyWindowItemOnGUI(int instanceID, Rect selectionRect)
+    {
+        var gameObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+
+        if (gameObject != null && gameObject.name.StartsWith("---", System.StringComparison.Ordinal))
+        {
+            EditorGUI.DrawRect(selectionRect, Color.blue);
+            EditorGUI.DropShadowLabel(selectionRect, gameObject.name.Replace("-", "").ToUpperInvariant());
+        }
+    }
+}
